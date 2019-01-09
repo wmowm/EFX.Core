bbbdd9395d64644417636e54e3659d7b9508428e
liu.liang <liu.liang@shuweikeji.com>
2019-01-09 18:38:19 +0800

    111

--- Test/Program.cs
+++ Test/Program.cs
@@ -27,23 +27,6 @@ namespace Test
 
             IBaseRepository<Manager> re = new DhmRepository<Manager>();
             var ll = re.GetList();
-
-
-            //string SavePath = @"F:\ExcelTemp\whitelist_sample.xlsx";
-            //Console.WriteLine("ETH地址             此日期前不可购买             此日期前不可售出             KYC/ AML有效期");
-
-            //DataTable dt = OfficeHelper.ReadExcelToDataTable(SavePath);
-            //for (int i = 0; i < dt.Rows.Count; i++)
-            //{
-            //    Console.WriteLine($"第{i}行数据:");
-            //    for (int j = 0; j < dt.Columns.Count; j++)
-            //    {
-
-            //        Console.Write(dt.Rows[i][j].ToString() + "             ");
-
-            //    }
-            //    Console.WriteLine("");
-            //}
             Console.Read();
         }
     }
